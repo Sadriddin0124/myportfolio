73ca34e28c223e07e7bae2114e4ad17dc19054c5
Your Name <you@example.com>
2024-10-09 16:49:59 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { ref } from 'vue';
+const data = ref([
+  {title: "salom", desc: "Assalomu Alaykum"},
+  {title: "hi", desc: "Assalamu Alaikum"},
+])
+
+const addedItem = {title: "salom", desc: "Assalomu Alaykum"}
+
+const AddItem = (item) => {
+  data.value.push(item)
+}
+</script>
+
+<template>
+  <div class="box">
+    <h1>Coming soon!!!</h1>
+  </div>
+</template>
+
+<style scoped>
+* {
+  padding: 0;
+  margin: 0;
+}
+
+.box {
+  width: 100%;
+  height: 100vh;
+  background: #000;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+h1 {
+  font-size: 34px;
+  color: white;
+  font-family: sans-serif;
+}
+</style>
